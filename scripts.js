2f2f31adbead01b9f1a8b54f724a615ba5006ba1
Roberta Ferreira <pamellaferreira.si@gmail.com>
2021-02-13 02:07:39 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -19,12 +19,22 @@ const Modal = {
     //estudar toogle e tentar substituir
 }
 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [] //transforma de string para array ou objeto
+    },
+
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions)) //transforma de array para string
+    }
+}
+
 //Eu preciso somar as entradas
 //depois eu preciso somar as saídas e
 //remover das entradas o valor das saídas
 //assim, eu terei o total
 const Transaction = {
-    all: [
+    /* all: [
         {
             description:'Luz',
             amount: -50001,
@@ -49,6 +59,10 @@ const Transaction = {
             date: '23/01/2021'
         }
     ],
+    
+    substituído pelo código abaixo all*/
+
+    all: Storage.get(),
 
     add(transaction){
         Transaction.all.push(transaction)
@@ -104,12 +118,13 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? "income": "expense"
         
         const amount = Utils.formatCurrency(transaction.amount)
@@ -121,7 +136,7 @@ const DOM = {
             <td class=${CSSclass}>${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         </tr>
         `
@@ -149,6 +164,24 @@ const DOM = {
 
 //trabalha com os sinais, remove caracteres especiais, formata em moeda...
 const Utils = {
+
+    formatAmount(value) {
+        
+        value = Number(value.replace(/\,\./g, "")) * 100
+
+        //também pode ser escrito como: value = Number(value) * 100
+        
+        return value
+    },
+
+    formatDate(date) {
+
+        const splittedDate = date.split("-")
+
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+
+    },
+
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
 
@@ -201,6 +234,26 @@ const Form = {
             } 
     },
 
+    formatValues() {
+        let {description, amount, date} = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    clearFields() {
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
     submit(event) {
         event.preventDefault()
 
@@ -210,35 +263,39 @@ const Form = {
             Form.validateFields()
 
             // Formatar os dados para salvar
-            // Form.formatData()
+            const transaction = Form.formatValues()
+
             // Salvar
+            Transaction.add(transaction)
+            
             // Apagar os dados do formulário
-            // Modal feche
-            // Atualizar a aplicação
+            Form.clearFields()
+
+            // Modal fecha
+            Modal.close()
+
 
         } catch (error) {
             alert(error.message)
         }
 
-    },
+    }
 
 }
 
 const App = {
     init() {
-
-        Transaction.all.forEach(transaction => {
-            DOM.addTransaction(transaction)
-        })
+        Transaction.all.forEach(DOM.addTransaction)
         
         DOM.updateBalance()
-             
+        
+        Storage.set(Transaction.all)
     },
 
     reload() {
         DOM.clearTransactions()
         App.init()
-    },
+    }
 }
 
 App.init()
